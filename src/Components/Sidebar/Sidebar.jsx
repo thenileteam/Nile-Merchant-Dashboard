39b0525669d3a87d7867297c0b86d5b9a4504e93
kuipid01 <kuipid01@gmail.com>
2024-12-05 15:14:36 +0100

    "Refactor Sidebar component: removed unnecessary whitespace, added eslint-disable comment, and reformatted code"

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,31 +1,28 @@
-import { nilelogowhite, } from "../../assets"
-import Links  from '../../Links'
-const Sidebar = ({sidebarOpen, closeSidebar}) => {
+/* eslint-disable react/prop-types */
+import { nilelogowhite } from "../../assets";
+import Links from "../../Links";
+const Sidebar = ({ sidebarOpen, closeSidebar }) => {
   return (
-      <>
-           {/* Overlay for small screens */}
-           {sidebarOpen && (
-            <div
-              className="fixed inset-0 bg-black opacity-50 lg:hidden"
-              onClick={closeSidebar}
-            ></div>
-          )}
+    <>
+      {/* Overlay for small screens */}
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50 lg:hidden"
+          onClick={closeSidebar}
+        ></div>
+      )}
 
-          {/* Sidebar */}
-          <div
-            className={`fixed top-0 left-0 h-full w-[290px] z-20 bg-[#004324] border-2 text-white p-5 transition-transform transform ${
-              sidebarOpen ? "translate-x-0" : "-translate-x-full"
-            } lg:translate-x-0`}
-          >
-            <img
-              src={nilelogowhite}
-              alt=""
-              className="w-[100px] flex"
-            />
-            <Links />
-          </div>
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-[290px] z-20 bg-[#004324] border-2 text-white p-5 transition-transform transform ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } lg:translate-x-0`}
+      >
+        <img src={nilelogowhite} alt="" className="w-[100px] flex" />
+        <Links />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ src/Components/dashboardlayout.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import Sidebar from "./Sidebar/Sidebar";
+
+// const DashboardLayout = ({children}) => {
+//   return <div>
+
+//     <Sidebar closeSidebar={+}/>
+//     <div>
+//         {children}
+//     </div>
+//   </div>;
+// };
+
+// export default DashboardLayout;
