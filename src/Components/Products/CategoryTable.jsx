798fef3cabe6110ece0df40ad68b9167fb575196
pejuameenat <pejuameenat@gmail.com>
2024-12-09 18:16:05 +0100

    spaced table out

--- src/Components/Products/CategoryTable.jsx
+++ src/Components/Products/CategoryTable.jsx
@@ -4,8 +4,8 @@ import DeleteProduct from "../PopupModals/DeleteProduct";
 const CategoryTable = ({ data }) => {
     const[count, setCount] = useState(0)
   return (
-    <section className="mt-4 max-w-[800px] mx-auto">
-      <table className="w-full">
+    <section className="mt-2 max-w-[800px] mx-auto">
+      <table className="w-full border-separate border-spacing-y-5">
         <thead>
           <tr className="text-left bg-[#EAF4E2]">
             <th className=" p-2 shadow-lg">Category </th>
@@ -18,7 +18,7 @@ const CategoryTable = ({ data }) => {
         <tbody>
           {data?.map((item) => {
             return (
-              <tr key={item.id} className="mt-4">
+              <tr   className="mt-4" key={item.id}>
                     <td className="bg-white p-2 text-[#6e6e6e] font-semibold capitalize lex items-center gap-2 justify-center">{item.category.name }</td>
                     <td className="bg-white p-2 text-[#6e6e6e] font-semibold capitalize">{item.description }</td>
                     <td className="bg-white p-2 text-[#6e6e6e] font-semibold  ">{ count} <button className="text-lightGreen font-semibold capitalize">view</button></td>
