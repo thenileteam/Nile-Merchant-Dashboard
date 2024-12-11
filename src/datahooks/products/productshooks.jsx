003661726dcb00cffcf85a3130103358d3ee3cf9
pejuameenat <pejuameenat@gmail.com>
2024-12-11 13:28:54 +0100

    changes placeholder for description

--- src/datahooks/products/productshooks.jsx
+++ src/datahooks/products/productshooks.jsx
@@ -115,7 +115,7 @@ export const useCreateNewCategory = (onSuccessCallback) => {
       const transformedData = {
         id: response.data._id,
         name: response.data.categoryName,
-        description: response.data.categoryDescription || 'No Description', 
+        description: response.data.categoryDescription || 'xxxxxxxxx', 
         products: response.data.products || [],
       };
       toast.success("Category Added Successfully");
