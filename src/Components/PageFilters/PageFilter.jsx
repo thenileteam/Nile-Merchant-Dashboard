5b1f9cecbb9d441c861b51928ff65e254583dda8
pejuameenat <pejuameenat@gmail.com>
2025-03-09 22:50:48 +0100

    date filters

--- src/Components/PageFilters/PageFilter.jsx
+++ src/Components/PageFilters/PageFilter.jsx
@@ -1,16 +1,22 @@
 import { FiSearch } from "react-icons/fi";
-
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { useRef } from "react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import {parseISO } from "date-fns";
 const PageFilters = ({
   filters,
   activeFilter,
   onFilterChange,
   searchPlaceholder,
   handleSearch,
+  selectedDate,
+handleDateFilter,
   page,
 }) => {
   return (
-    <div className="flex justify-between mt-8 mb-4">
-      <div className="lg:flex lg:items-center space-x-4 border rounded-xl hidden ">
+    <div className="flex  flex-col lg:flex-row justify-between mt-8 mb-4 gap-4 lg:gap-0">
+      <div className="lg:flex lg:items-center space-x-4 border rounded-xl  ">
         {/* Filter Buttons */}
         {filters.map((filter, index) => (
           <button
@@ -36,6 +42,18 @@ const PageFilters = ({
           onChange={(e) => handleSearch(e.target.value)}
         />
         <FiSearch className="absolute top-3 left-3" />
+      </div>
+      <div className="hidden lg:block relative text-lightGreen lg:w-[150px]">
+      <DatePicker
+          selected= {selectedDate ? parseISO(selectedDate) : null} 
+          onChange={ handleDateFilter}
+          placeholderText="Select Date"
+          className="w-full border-2 rounded-md pl-8 py-2 cursor-pointer text-lightGreen font-semibold"
+          dateFormat="yyyy-MM-dd"
+          showPopperArrow={false}
+          popperClassName="!z-[1000]"
+        />
+        <FaRegCalendarAlt className="absolute left-3 top-3" />
       </div>
     </div>
   );
--- src/Components/PageFilters/usePageFilter.jsx
+++ src/Components/PageFilters/usePageFilter.jsx
@@ -1,65 +1,75 @@
 import { useState, useMemo } from "react";
-
- export const useFilter = (data, filterFunction) => {
-    const [selectedFilter, setSelectedFilter] = useState("All");
-    const [searchTerm, setSearchTerm] = useState("");
-  
-    const handleActiveFilterChange = (filter) => {
-      setSelectedFilter(filter);
-    };
-  
-    const handleSearch = (query) => {
-      setSearchTerm(query);
-    };
-  
-    // Filter data based on selected filter and search term
-    const filteredItems = useMemo(() => {
-      if (!data) return [];
-  
-      let filterItems = data;
-  
-      if (filterFunction) {
-        filterItems = filterFunction(filterItems, selectedFilter);
-      }
-  
-      if (searchTerm) {
-        filterItems = filterItems.filter((item, i) => {
-          const lowerCaseQuery = searchTerm.toLowerCase();
-           
-          return (
-            (item.id && item.id.toString().includes(lowerCaseQuery)) ||
-            (item.name && item.name.toLowerCase().includes(lowerCaseQuery)) ||
-            (item.price && item.price.toString().includes(lowerCaseQuery)) ||
-              (item.email && item.email.toLowerCase().includes(lowerCaseQuery)) 
-            //    (item.items[i]?.name && item.items[i]?.name.toLowerCase().includes(lowerCaseQuery)) ||
-            //    (item.customer[i]?.name && item.customer[i]?.name.toLowerCase().includes(lowerCaseQuery))
-              
-          );
-        });
-        }
-      return filterItems
-    }, [data, selectedFilter, searchTerm, filterFunction]); 
-    const noResults = filteredItems.length === 0;
-    return {
-      selectedFilter,
-      searchTerm,
-      handleActiveFilterChange,
-      handleSearch,
-      filteredItems,
-      noResults
-    };
+import { format, parseISO } from "date-fns"
+export const useFilter = (data, filterFunction) => {
+  const [selectedFilter, setSelectedFilter] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+  const handleActiveFilterChange = (filter) => {
+    setSelectedFilter(filter);
   };
-  
+
+  const handleSearch = (query) => {
+    setSearchTerm(query);
+  };
+  const handleDateFilter = (date) => {
+    setSelectedDate(date);
+    console.log(selectedDate)
+  };
+  // Filter data based on selected filter and search term
+  const filteredItems = useMemo(() => {
+    if (!data) return [];
+
+    let filterItems = data;
+
+    if (filterFunction) {
+      filterItems = filterFunction(filterItems, selectedFilter);
+    }
+
+    if (searchTerm) {
+      filterItems = filterItems.filter((item, i) => {
+        const lowerCaseQuery = searchTerm.toLowerCase();
+
+        return (
+          (item.id && item.id.toString().includes(lowerCaseQuery)) ||
+          (item.name && item.name.toLowerCase().includes(lowerCaseQuery)) ||
+          (item.price && item.price.toString().includes(lowerCaseQuery)) ||
+          (item.email && item.email.toLowerCase().includes(lowerCaseQuery))
+          //    (item.items[i]?.name && item.items[i]?.name.toLowerCase().includes(lowerCaseQuery)) ||
+          //    (item.customer[i]?.name && item.customer[i]?.name.toLowerCase().includes(lowerCaseQuery))
+        );
+      });
+    }
+    //    date filters
+    if (selectedDate) {
+      filterItems = filterItems.filter((item) => {
+        if (!item.createdAt) return false; //  if theres an item
+        const itemDate = parseISO(item.createdAt);
+        return format(itemDate, 'yyyy-MM-dd') === format(selectedDate, 'yyyy-MM-dd');
+      });
+    }
+    return filterItems;
+  }, [data, selectedFilter, searchTerm, selectedDate, filterFunction]);
+  const noResults = filteredItems.length === 0;
+  return {
+   selectedDate: selectedDate ? format(selectedDate, 'yyyy-MM-dd') : '',
+    selectedFilter,
+    searchTerm,
+    handleActiveFilterChange,
+    handleSearch,
+    handleDateFilter,
+    filteredItems,
+    noResults,
+  };
+};
+
 //   check recent customer;
 export const isRecentCustomer = (customer) => {
-    if (!customer.createdAt) return false;  
-    
-    const createdDate = new Date(customer.createdAt);
-    const today = new Date();
-    // Calculate the difference in days
-    const diffInDays = (today - createdDate) / (1000 * 60 * 60 * 24);
-  
-    return diffInDays <= 7;
-  };
-  
-  
+  if (!customer.createdAt) return false;
+
+  const createdDate = new Date(customer.createdAt);
+  const today = new Date();
+  // Calculate the difference in days
+  const diffInDays = (today - createdDate) / (1000 * 60 * 60 * 24);
+
+  return diffInDays <= 7;
+};
